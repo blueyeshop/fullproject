4a95803a379dc1457648f7c3f659936c3ff54829
blueyeshop <blueye.shop.bd@gmail.com>
2023-02-05 18:47:03 +0600

    React-basic-structure

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">Blueye.shop</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
